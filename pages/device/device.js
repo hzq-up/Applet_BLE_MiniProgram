e228cfa701db45ed3594cfd3d2071e084975a53f
huzhiqiang <hzq_up@126.com>
2020-05-30 02:18:24 +0800

    完善代码，提取方法

--- pages/device/device.js
+++ pages/device/device.js
@@ -7,7 +7,11 @@ Page({
     connectedDeviceId: '',
     services: {},
     characteristics: {},
-    connected: true
+    connected: true,
+    serviceIndex: 0,
+    notify: false,
+    write: false,
+    read: false
   },
   bindInput: function (e) {
     this.setData({
@@ -27,15 +31,14 @@ Page({
 
       wx.writeBLECharacteristicValue({
         deviceId: that.data.connectedDeviceId,
-        serviceId: that.data.services[1].uuid,
-        characteristicId: that.data.characteristics[3].uuid,
+        serviceId: app.BLEInformation.writeServiceId,
+        characteristicId: app.BLEInformation.writeCharaterId,
         value: buffer,
         success: function (res) {
           console.log('发送成功')
         }
       })
-    }
-    else {
+    } else {
       wx.showModal({
         title: '提示',
         content: '蓝牙已断开',
@@ -67,44 +70,70 @@ Page({
   // 获取蓝牙设备某个服务中支持notify 或者 indicate的特征值
   getCharacteristics: function () {
     var that = this;
-    var index = 0;
-    var notify = false;
+    var index = that.data.serviceIndex;
+    var notify = that.data.notify;
+    var write = that.data.write;
+    var read = that.data.read;
     wx.getBLEDeviceCharacteristics({
       deviceId: that.data.connectedDeviceId,
       serviceId: that.data.services[index].uuid,
       success: function (res) {
-        console.log(res.characteristics)
-        // console.log(that.data.services)
-        // that.setData({
-        //   characteristics: res.characteristics
-        // })
-
+        // console.log(res.characteristics)
         for (var i = 0; i < res.characteristics.length; ++i) {
           var properties = res.characteristics[i].properties;
-          var notifyCharaterId = res.characteristics[i].uuid;
-          if (properties.notify) {
-            app.BLEInformation.notifyCharaterId = notifyCharaterId;
-            notify = true;
-            break;
-          }else {
-            // if(index == that.data.services.length) {
-            notify = false
-            // }
+          var charaterId = res.characteristics[i].uuid;
+
+          if (!notify) {
+            if (properties.notify) {
+              app.BLEInformation.notifyCharaterId = charaterId;
+              app.BLEInformation.notifyServiceId = that.data.services[index].uuid;
+              notify = true;
+            }
+          }
+          if (!write) {
+            if (properties.write) {
+              app.BLEInformation.writeCharaterId = charaterId;
+              app.BLEInformation.writeServiceId = that.data.services[index].uuid;
+              write = true;
+            }
+          }
+          if (!read) {
+            if (properties.read) {
+              app.BLEInformation.readCharaterId = charaterId;
+              app.BLEInformation.readServiceId = that.data.services[index].uuid;
+              read = true;
+            }
           }
         }
-        if(notify) {
-          app.BLEInformation.notifyServiceId = that.data.services[index].uuid;
-        }else {
-          index++;
-          that.getCharacteristics();
-          if(index == that.data.services.length) {
+        if (!write || !notify || !read) {
+          index++
+          that.setData({
+            write: write,
+            read: read,
+            notify: notify,
+            serviceIndex: index
+          })
+          if (index == that.data.services.length) {
+            // that.setData({
+            //   write: false,
+            //   read: false,
+            //   notify: false,
+            //   serviceIndex: 0
+            // })
             wx.showModal({
               title: '提示',
               content: '找不到该读写的特征值',
             })
+          } else {
+            that.getCharacteristics();
           }
+        } else {
+          console.log(res.characteristics);
+          console.log("write:"+app.BLEInformation.writeCharaterId)
+          console.log("read:"+app.BLEInformation.readCharaterId)
+          console.log("notify:"+app.BLEInformation.notifyCharaterId)
+          that.monitorCharacteristicValueChange();
         }
-
       }
     })
   },
@@ -121,14 +150,24 @@ Page({
       success: function (res) {
         // console.log('启用notify成功')
         console.log('notifyBLECharacteristicValueChange success', res);
-        wx.onBLECharacteristicValueChange(function (res) {
-          console.log(res)
-          var receiveText = app.buf2string(res.value)
+        wx.onBLECharacteristicValueChange(function (r) {
+          console.log(r)
+          var receiveText = app.buf2string(r.value)
           console.log('接收到数据：' + receiveText)
           that.setData({
             receiveText: receiveText
           })
         })
+        // setTimeout(function () {
+        //   wx.onBLECharacteristicValueChange(function (r) {
+        //     console.log(r)
+        //     var receiveText = app.buf2string(r.value)
+        //     console.log('接收到数据：' + receiveText)
+        //     that.setData({
+        //       receiveText: receiveText
+        //     })
+        //   })
+        // }, 1000);
       }
     })
   },
@@ -141,7 +180,6 @@ Page({
     })
     that.getNotifyCharaterId();
     // 监听蓝牙设备发送的数据
-    that.monitorCharacteristicValueChange();
     wx.onBLEConnectionStateChange(function (res) {
       console.log(res.connected)
       that.setData({
